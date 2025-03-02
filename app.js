7139fbda942659f2df086e6b009bd92006d4ab82
AnushaRenangi <anusharenangi@gridlex.com>
2025-03-02 21:26:12 +0530

    fix:Cors

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ let app=express();
 
 app.use(express.json());
 
+app.use(cors());
+
 if(process.env.NODE_ENV === 'development')
 {
         app.use(morgan('dev'));
@@ -33,8 +35,6 @@ app.use('/api/v1/auth',authRouter);
 app.all('*',(req,res,next)=>{
         const err=new customError(`can't find this url${req.originalUrl}`,404);
         next(err);
-
-
 })
 
 app.use(GlobalErrorHandler);
